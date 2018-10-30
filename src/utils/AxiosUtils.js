843d484bb202b3b20df1af882ea41ecdef1cbdf9
chenliwu <30075213@qq.com>
2018-10-30 23:06:29 +0800

    Axios

--- src/utils/AxiosUtils.js
+++ src/utils/AxiosUtils.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 /**
  * 设置全局的 axios 默认值
+ * 
  */
 const axiosInstance = axios.create({
-    baseURL:"http://10.0.2.2:8099/MySSM",   //设置访问URL前缀
+    baseURL: "http://10.0.2.2:8099/MySSM",   //设置访问URL前缀
     timeout: 2000,  //`timeout` 指定请求超时的毫秒数(0 表示无超时时间)
 });
 
